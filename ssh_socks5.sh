cf3a7103bbb561dfea2f2c3533bee948e39457c6
龙登余 <Longdengyu@users.noreply.github.com>
2020-06-14 13:23:01 +0800

    ssh_socks5.sh
    socks5 local server by ssh

---
+++ ssh_socks5.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##
+#  Description: socks5 local server by ssh
+# 
+#  Note: for the safety you can use public key authentication by adding your 
+#   rsa pub key to .ssh/authorized_keys, then adding directives to the pub key 
+#   like this 'restrict,port-forwarding,command="/bin/false" ssh-rsa AAAAB3NzaC1...'
+##
+
+
+REMOTE_USER=<user>
+REMOTE_HOST=<host>
+REMOTE_PORT=<port>
+
+trap exit SIGINT 
+trap exit SIGTERM
+
+while true
+do 
+    echo running netcat guard
+    echo this is netcat guard | nc -lvp 1080
+
+    echo running ssh socks5
+    ssh -D 0.0.0.0:1080  -C -q -N $REMOTE_USER@$REMOTE_HOST -p $REMOTE_PORT
+    echo
+done
+
