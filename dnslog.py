febb6cc8f25acd5f21c7c0aea40178ca865eec8c
龙登余 <Longdengyu@users.noreply.github.com>
2020-07-25 14:56:17 +0800

    Update dnslog.py

--- dnslog.py
+++ dnslog.py
@@ -7,12 +7,10 @@
 # Date Sat Jul 25 02:46:40 EDT 2020
 #
 
-
 import sys
 import socket
 from datetime import datetime
 from struct import unpack
-
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('0.0.0.0', 53)
@@ -32,9 +30,7 @@ def parsedns(req_bytes):
         count_index = end
 
         domain_splices.append(str(req_bytes[start:end]))
-
         count = int(unpack('B',req_bytes[count_index])[0])
-
         if count_index + count + 1 >len(req_bytes):
             return None
         elif count == 0:
@@ -44,8 +40,8 @@ def parsedns(req_bytes):
 while True:
     print('\nwaiting to receive message')
     sys.stdout.flush()
+    
     data, address = sock.recvfrom(4096)
-
     print('date: ' + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
     print('received %s bytes from %s' % (len(data), address))
     print("DNS request: " + parsedns(data))
