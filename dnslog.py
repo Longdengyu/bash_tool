6bdbbbb03a0cf0df411376d1b268201f7e3d3659
龙登余 <Longdengyu@users.noreply.github.com>
2020-07-27 17:49:34 +0800

    Update dnslog.py
    better parse

--- dnslog.py
+++ dnslog.py
@@ -18,23 +18,23 @@ print('starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
 def parsedns(req_bytes):
-    if len(req_bytes) < 1:
-        return "";
-    count = int(unpack('B',req_bytes[0])[0])
-    if count == 0:
-        return ""
-    if count > len(req_bytes) -1:
-        return ""
-    return req_bytes[1:count + 1] + "." + parsedns(req_bytes[count+1:])
+    def _parsedns(req_bytes):
+        count = int(unpack('B',req_bytes[0])[0])
+        if count == 0:
+            return ""
+        return req_bytes[1:count + 1] + "." + _parsedns(req_bytes[count+1:])
+
+    try:
+        return _parsedns(req_bytes[12:])[:-1]
+    except Exception:
+        return "Error"
 
 while True:
     print('\nwaiting to receive message')
     sys.stdout.flush()
     data, address = sock.recvfrom(4096)
-    if len(data) < 13:
-        continue
 
     print('date: ' + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
     print('received %s bytes from %s' % (len(data), address))
-    print("DNS request: " + parsedns(data[12:]))
+    print("DNS request: " + parsedns(data))
     sys.stdout.flush()
