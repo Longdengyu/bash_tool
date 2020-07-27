fb2b2c42ca7765ddfd7194f4ae059ab80a263155
龙登余 <Longdengyu@users.noreply.github.com>
2020-07-27 14:07:22 +0800

    Update dnslog.py
    parsedns using recursive method

--- dnslog.py
+++ dnslog.py
@@ -18,31 +18,21 @@ print('starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
 def parsedns(req_bytes):
-    domain_splices=[]
-    count_index=12
-    count = int(unpack('B',req_bytes[count_index])[0])
+    if len(req_bytes) < 1:
+        return "";
+    count = int(unpack('B',req_bytes[0])[0])
     if count == 0:
-        return None
-
-    while count > 0:
-        start = count_index + 1
-        end = start + count
-        count_index = end
-
-        domain_splices.append(str(req_bytes[start:end]))
-        count = int(unpack('B',req_bytes[count_index])[0])
-        if count_index + count + 1 >len(req_bytes):
-            return None
-        elif count == 0:
-            break
-    return ".".join(domain_splices)
+        return ""
+    if count > len(req_bytes) -1:
+        return ""
+    return req_bytes[1:count + 1] + "." + parsedns(req_bytes[count+1:])
 
 while True:
     print('\nwaiting to receive message')
     sys.stdout.flush()
-    
     data, address = sock.recvfrom(4096)
+
     print('date: ' + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
     print('received %s bytes from %s' % (len(data), address))
-    print("DNS request: " + parsedns(data))
+    print("DNS request: " + parsedns(data[12:]))
     sys.stdout.flush()
