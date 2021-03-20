2bcfc36f2813e61a7b43811fe85cb8af4a823d29
龙登余 <Longdengyu@users.noreply.github.com>
2021-03-20 14:02:04 +0800

    Update dnslog.py
    make python3 happy

--- dnslog.py
+++ dnslog.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #
 # Filename: dnslog.py
 # Description: dummy DNS server for outbound
 # Usage: python dnslog.py
-# Date: Mon Jul 27 02:48:02 EDT 2020
 #
 
 import sys
 import socket
-from datetime import datetime
+from datetime import datetime as dt
 from struct import unpack
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -19,19 +18,25 @@ sock.bind(server_address)
 
 def parsedns(req_bytes):
     def _parsedns(req_bytes):
-        count = int(unpack('B',req_bytes[0])[0])
-        return "" if count == 0 else req_bytes[1:count + 1] + "." + _parsedns(req_bytes[count+1:])
+        count = req_bytes[0]
+        if count == 0:
+            return ""
+        return req_bytes[1:count + 1].decode() + "." + _parsedns(req_bytes[count+1:])
+
     try:
         return _parsedns(req_bytes[12:])[:-1]
-    except Exception:
+    except Exception as e:
         return "Error"
 
-while True:
-    print('\nwaiting to receive message')
-    sys.stdout.flush()
-    data, address = sock.recvfrom(4096)
+def dnslog_serve_forever():
+    while True:
+        print('\nwaiting to receive message')
+        sys.stdout.flush()
+        data, address = sock.recvfrom(4096)
 
-    print('date: ' + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
-    print('received %s bytes from %s' % (len(data), address))
-    print("DNS request: " + parsedns(data))
-    sys.stdout.flush()
+        print('date: ' + dt.now().strftime("%Y/%m/%d %H:%M:%S"))
+        print('received %s bytes from %s' % (len(data), address))
+        print("DNS request: " + parsedns(data))
+        sys.stdout.flush()
+
+dnslog_serve_forever()
